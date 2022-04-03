e03de0a5cdebec8dec08fa1285da6a143691d4cc
Pradhumna Singh <89626211+PradhumnaSingh02@users.noreply.github.com>
2022-04-03 22:36:44 +0530

    Add files via upload

---
+++ gui12.py
@@ -0,0 +1,12 @@
+#Scale function is used to create a slider
+from tkinter import *
+
+root = Tk()
+root.geometry('644x456')
+root.title('Scale')
+
+#slider1 = Scale(root, from=0, to=46, orient=HORIZONTAL)
+#slider1.pack()
+
+
+root.mainloop()
